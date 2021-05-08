d8e5f92d62fab7268db7ba105164bf915cdb2761
Thomas Simpson <thomas.simpson768@gmail.com>
2021-05-08 17:12:57 +0100

    Added play method

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,21 @@
 class Game
-  def initialize
-    
+  def initialize(board = Board.new)
+    @board = board
   end
 
+  # Main game loop. This is a looping script that
+  # puts Question for input
+  # Get and validate input is valid. 
+  # Update the board.
+  # Check if the game has been won. If so, break
+  # Update the current player.
   def play
-    
+    loop do
+      input = parse_input
+      @board.insert(input)
+      break if @board.won?
+
+      update_player
+    end
   end
 end
