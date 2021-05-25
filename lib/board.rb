0bbaddc363d171a11e7a75596d759b1d1fbc947e
Thomas Simpson <thomas.simpson768@gmail.com>
2021-05-25 17:32:51 +0100

    Detect a win on horizontal line

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,19 @@
+
+# Board of a connect 4 game. @board represents the board
+# in the order
+# 35, 36, 37, 38, 39, 40, 41
+# 28, 29, 30, 31, 32, 33, 34
+# 21, 22, 23, 24, 25, 26, 27
+# 14, 15, 16, 17, 18, 19, 20
+#  7,  8,  9, 10, 11, 12, 13
+#  0,  1,  2,  3,  4,  5,  6
 class Board
-  def initialize
-    @board = Array.new(42, nil)
+  COUNTERS_TO_WIN = 4
+  def initialize(columns = 7, rows = 6)
+    @columns = columns
+    @rows = rows
+    num_spaces = columns * rows
+    @board = Array.new(num_spaces, nil)
   end
 
   def insert(column, counter)
@@ -13,6 +26,35 @@ class Board
   end
 
   def won?
+    winning_lines = winning_rows # I really want this to be a constant, set at initiazation
+    winning_lines.each do |line|
+      return true if line.all? { |pos| !@board[pos].nil? && @board[pos] == @board[line[0]] }
+    end
+    false
+  end
+
+  private
+
+  def winning_rows
+    rows = []
+    (0..3).each do | number |
+      rows.push([0,1,2,3].map {|win_number| win_number + number})
+    end
+    rows_to_return = rows.reduce([]) { | winning_rows, row | winning_rows.push(all_alligned_rows(row))}
+    rows_to_return.flatten(1)
+  end
+
+  def all_alligned_rows(row)
+    (0..@rows-1).reduce([]) do |alligned_rows, row_height |
+      alligned_rows.push(row.map {|row_number| row_number + row_height * 7})
+    end
+  end
+
+  def winning_columns
+    
+  end
+
+  def winning_diagonals
     
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,7 @@
 require '../lib/board'
 describe Board do
+  counter_red = '🔴'
+  counter_blue = '🔵'
   describe '#insert' do
     context 'When board is empty' do
       subject(:empty_board) { described_class.new }
@@ -14,8 +16,7 @@ describe Board do
     context 'When inserting into a column with 1 counter' do
       subject(:part_filled_board) { described_class.new }
       input = 5
-      counter_red = '🔴'
-      counter_blue = '🔵'
+
       board = Array.new(42, nil)
       board[input - 1] = counter_blue
       expected_board = board.clone
@@ -40,6 +41,47 @@ describe Board do
   end
 
   describe 'won?' do
-    
+    context 'when board is empty' do
+      subject(:empty_board) {described_class.new}
+      it 'returns false' do
+        expect(empty_board).not_to be_won
+      end
+    end
+    context 'When there is 4 same coloured counters in a row' do
+      subject(:board_row) {described_class.new}
+      before do
+        board = Array.new(42, nil)
+        board[0] = counter_blue
+        board[1]= counter_blue
+        board[2] = counter_blue
+        board[3] = counter_blue
+        board_row.instance_variable_set(:@board, board)
+      end
+      it 'returns true' do
+        expect(board_row).to be_won
+      end
+    end
+  
+    context 'when there is 4 same coloured counters in a column' do
+      xit 'returns true' do
+        
+      end
+    end
+    context 'When there is 4 same coloured counters on a diagonal' do
+      xit 'returns true' do
+        
+      end  
+    end
+    context 'When a game is in progress with no winning lines' do
+      xit 'returns false' do
+        
+      end
+    end
+    context 'When a board is full with no wins' do
+      xit 'returns false' do
+        
+      end
+    end
   end
+
 end
